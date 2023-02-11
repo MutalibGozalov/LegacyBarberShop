4cf41c67873bffe3f50f7a3690e6da6c6ab9cbdd
Mutalib Gozalov <mg@mail.com>
2023-02-11 19:27:46 +0400

    3 files replaced

--- build.sh
+++ build.sh
@@ -2,12 +2,9 @@
 # exit on error
 set -o errexit
 
-
 poetry install
-pip install setuptools
+pip install --force-reinstall -U setuptools
 python manage.py collectstatic --no-input
 python manage.py migrate
 
 python manage.py createsuperuser --no-input
-
-# --force-reinstall -U
